e4b3129ae99da6fcbb9c0b001543ed06419bbcce
tareq-salem <38789773+tareq-salem@users.noreply.github.com>
2018-11-19 14:41:54 +0100

    test

--- src/main/java/fr/formation/security/JwtAuthenticationFilter.java
+++ src/main/java/fr/formation/security/JwtAuthenticationFilter.java
@@ -26,6 +26,9 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
 
 		SecurityContextHolder.getContext().setAuthentication(authentication);
 		filterChain.doFilter(request, response);
+
+
+		
 	}
 
 }
